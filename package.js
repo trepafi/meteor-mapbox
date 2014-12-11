b1bdc3af782ea01b0248f886cf3786498ef75bc8
Lt <trepafi@gmail.com>
2014-12-11 23:49:19 +0100

    Updating package

--- package.js
+++ package.js
@@ -1,19 +1,13 @@
 Package.describe({
   name: 'trepafi:mapbox',
   summary: 'Integrating MapBox.js with Meteor  ',
-  version: '0.0.3',
+  version: '0.1.0',
   git: 'https://github.com/trepafi/meteor-mapbox.git'
 });
 
 Package.onUse(function(api) {
-  api.versionsFrom('1.0');
-  api.use(['tracker', 'underscore'], ['client']);
-  api.addFiles(['mapbox.js'], ['client']);
-  api.export('Mapbox', ['client']);
-});
-
-Package.onTest(function(api) {
-  api.use('tinytest');
-  api.use('trepafi:mapbox');
-  api.addFiles('mapbox-tests.js');
+  api.versionsFrom('METEOR@1.0');
+  api.use("jquery", 'client');
+  api.use("mizzao:build-fetcher@0.2.0", 'client');
+  api.addFiles('mapbox.fetch.json', 'client');
 });
